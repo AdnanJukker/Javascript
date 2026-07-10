7068d2342f74b6e06fffaed0b02ee247f0dc0014
Adnan Jukker <93384175+AdnanJukker@users.noreply.github.com>
2026-07-10 19:30:21 +0530

    Add files via upload

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,29 @@
+let score = "33aabc"
+
+// console.log(score)
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueOfNumber = Number(score) //explicit conversion
+// console.log(valueOfNumber)
+// console.log(typeof valueOfNumber)
+
+// "33" => 33
+//"33avcbc" => NaN
+//ture => 1; false =>0
+
+let isLoggedIn = 0
+
+let booleanisLoggegIn = Boolean(isLoggedIn)
+
+// console.log(booleanisLoggegIn)
+
+//1 => true; 0 => false
+//""=> false; "adnan" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber)
+console.log(typeof stringNumber)
