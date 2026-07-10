dd7fab4efb757e6926b68a0106f0578742fb52a2
Adnan Jukker <93384175+AdnanJukker@users.noreply.github.com>
2026-07-10 19:50:08 +0530

    Add files via upload

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -25,5 +25,32 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber)
-console.log(typeof stringNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+// ***************************************operations***************************************
+
+
+let value = 3
+let neg = -value
+// console.log(neg)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/2)
+// console.log(2%2)
+
+
+let str1 = "Adnan"
+let str2 = " jukker"
+let str3 = str1 + str2
+// console.log(str3)
+
+
+//console.log("1"+2)
+//console.log(1+"2")
+//console.log("1"+2+2)
+//console.log("1"+(2+2))
+
