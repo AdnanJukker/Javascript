1a8eb800bf4e7bf59fe2ff7fca1fd28dce38cc23
Adnan Jukkerwalla <adnanjukker402@gmail.com>
2026-07-13 23:47:41 +0530

    Add initial JavaScript files for variables, data types, conversion operations, comparisons, memory types, strings, and math operations

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,23 @@
+const accountid = 1234
+let accountemail = "adnan@eg.com"
+var accountpassword = "1234"
+accountcity ="udaipur"
+let accountState
+
+// accountid = 6748
+
+accountemail = "abd@edg.com"
+accountpassword = "4356"
+accountcity = "jaipur"
+
+
+console.table([accountid, accountemail, accountpassword, accountcity,accountState])
+
+
+// Variables and Data Types
+// const = can't be re-assigned a value and can't be declared again  (block scope)
+// let = can be re-assigned a value but can't be declared again (block scope)
+// var = it can be re-assigned a value and it can also be declared again  (Global scope)
+
+// => Never use var in your code instead of let because of block and functional scope issues.
+// # console.table([group of variables you want to show in tabular form]) 
