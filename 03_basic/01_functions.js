af9a283309cd9cb4db202303c6fc3f4fdf7d46cc
Adnan Jukkerwalla <adnanjukker402@gmail.com>
2026-07-15 23:27:07 +0530

    Add functions for adding two numbers and user login message

---
+++ 03_basic/01_functions.js
@@ -0,0 +1,24 @@
+// function addTwoNumb(num1,num2){ //passing the parameters
+//     console.log(num1+num2);
+// }
+
+// addTwoNumb(4,7) //passing the arguments
+
+function addTwoNumb(num1,num2){ 
+    // let result = num1+num2
+    // return result
+    return num1+num2
+}
+const result = addTwoNumb(4,7) 
+
+// console.log("result: ",result);
+
+function loginUserMsg(username = "Papita"){
+    if(!username){
+        console.log("Please enter your username");
+        return
+    }
+    return `${username} just logged in`
+}
+console.log(loginUserMsg())
+
