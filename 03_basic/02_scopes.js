d498812534b05fed7202632c0cea9a4da029f228
Adnan Jukkerwalla <adnanjukker402@gmail.com>
2026-07-16 23:48:24 +0530

    Refactor scope examples and clean up commented code

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -10,6 +10,45 @@ if(true){ //block scope
     
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+//nested scoping 
+
+function one(){
+    const username = "Adnan"
+
+    function two(){
+        const website = "abc.com"
+        console.log(username)
+    }
+    // console.log(website);
+    
+    two()
+}
+// one()
+
+if(true){
+    const username = "adnan"
+    if(username === "adnan"){
+        const website = " abc.com"
+        console.log(username + website)
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+//hoisting 
+
+console.log(addone(5));
+
+function addone(num){
+    return num +1
+}
+
+const addtwo = function(num){ //hoisting 
+    return num + 2
+}
+console.log(addtwo(5));
