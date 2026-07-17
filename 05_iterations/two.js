09e270d6d078bcfd01a7f0023f7df976b48b78dc
Adnan Jukkerwalla <adnanjukker402@gmail.com>
2026-07-17 17:45:06 +0530

    Add while and do-while loop examples with array traversal and score logging

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+// let i = 0;
+// while (i <=10) {
+//     console.log(`value of index is ${i}`);
+//     i = i+2;
+// }
+
+let myArr = ['flash','ironman','batman']
+arr = 0;
+while (arr < myArr.length) {
+    // console.log(`value is ${myArr[arr]}`);
+    arr = arr +1
+}
+
+let score = 0;
+
+do {
+    console.log(`score is ${score}`);
+    score++
+    
+} while (score <= 10);
